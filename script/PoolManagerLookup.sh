8af61b23c7fd79e740d03eb8451f601510a8a5e1
Paul Reinholdtsen <paul@bitsy.services>
2026-05-02 17:33:55 -0500

    PoolManagerLookup

--- script/PoolManagerLookup.sh
+++ script/PoolManagerLookup.sh
@@ -3,19 +3,19 @@ initcode="0x3d602d80600a3d3981f3363d3d373d3d3d363d73${deployer#0x}5af43d82803e90
 initcodehash=$(cast keccak "$initcode")
 echo "initcodehash=$initcodehash"
 
-kvs=$(yq --from-file script/kvs.yq script/USDC.yml | paste -sd,)
+kvs=$(property=PoolManager yq --from-file script/uniswap.yq script/uniswap.yml)
 argshash=$(cast keccak "$(cast abi-encode "f((uint256,address)[])" "[$kvs]")")
 echo "argshash=$argshash"
 
-variant=0x000000000000000000000000000000000000000000000000000000007fa78ab1
+variant=0x000000000000000000000000000000000000000000000000000000016c3e78be
 input=$(cast calldata "make((uint256,address)[],uint256)" "[$kvs]" "$variant")
 # XOR argshash ^ variant (256-bit, too wide for bash arithmetic)
 salt=$(python3 -c "print(f'0x{int(\"$argshash\",16) ^ int(\"$variant\",16):064x}')")
 home=$(cast create2 --deployer "$deployer" --salt "$salt" --init-code "$initcode")
 echo "home=$home"
 
-mkdir -p io/USDC
-printf '%s' "$input" > "io/USDC/$home.txt"
+mkdir -p io/PoolManagerLookup
+printf '%s' "$input" > "io/PoolManagerLookup/$home.txt"
 
-target=0xc5dc00000000000000000000000000000000e210
+target=0xb00100000000000000000000000000000000e210
 mask=0xffff00000000000000000000000000000000ffff
