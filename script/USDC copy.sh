aa40a08481ea4a198ade5af626759d9a1c47dd80
Paul Reinholdtsen <paul@bitsy.services>
2026-05-02 16:43:48 -0500

    USDC=0xc5dc7c969fa0a50561c5a48307BeA0316F07C5Dc

--- script/USDC copy.sh
+++ script/USDC copy.sh
@@ -1,5 +1,6 @@
-variant=0x00000000000000000000000000000000000000000000000000000000a2887718
+variant=0x0000000000000000000000000000000000000000000000000000000000000000
 kvs=$(yq --from-file script/kvs.yq script/USDC.yml | paste -sd,)
+echo "kvs="$kvs""
 deployer=0xAdD181fDf0E1077Bcb846ce03B99850Bca1de210
 supply=3141592653589793238
 target=0xc5dc00000000000000000000000000000000c5dc
@@ -8,7 +9,7 @@ mask=0xffff00000000000000000000000000000000ffff
 initcode="0x3d602d80600a3d3981f3363d3d373d3d3d363d73${deployer#0x}5af43d82803e903d91602b57fd5bf3"
 initcodehash=$(cast keccak "$initcode")
 
-argshash=$(cast keccak "$(cast abi-encode "f((uint256,address)[])" "[$kvs]")")
+argshash=$(cast keccak "$(cast abi-encode "f((uint256,address)[])" "$kvs")")
 
 # XOR argshash ^ variant (bc has no XOR, so use python)
 salt=$(python3 -c "print(f'0x{int(\"$argshash\",16) ^ int(\"$variant\",16):064x}')")
@@ -17,7 +18,7 @@ home=$(cast create2 --deployer "$deployer" --salt "$salt" --init-code "$initcode
 input=$(cast calldata "make((uint256,address)[],uint256)" "[$kvs]" "$variant")
 
 echo "initcodehash=$initcodehash"
-echo "argshash=$argshash"
+echo "salt=$salt"
 echo "home=$home"
 
 mkdir -p io/USDC
