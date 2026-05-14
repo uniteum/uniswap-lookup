579251cd81d3c3ac1023edc3a23a799869d1441a
Paul Reinholdtsen <paul@bitsy.services>
2026-05-14 12:33:53 -0500

    Mine USDC 0xC5dc93F7899E38E4ab2804E556E74fbC6709E300

--- io/USDC/USDC.sh
+++ io/USDC/USDC.sh
@@ -10,7 +10,7 @@ source "$(git rev-parse --show-toplevel)/lib/crucible/script/clone.sh"
 
 # Locale's AddressLookup prototype (same prototype used by
 # io/PoolManagerLookup/PoolManagerLookup.sh).
-deployer=0xADD27841708048b2B8F053A9199f320A0724e300
+deployer=0xaDDb841F7f2D4176C480E3734448047ad83FE300
 
 # (chainId, USDC) pairs from script/USDC.yml, formatted as a Solidity
 # tuple-array literal: "(1,0x...),(8453,0x...),...".
@@ -24,4 +24,4 @@ target=0xc5dc00000000000000000000000000000000e300
 
 clone_predict USDC "$deployer" \
     "(uint256,address)[]" "[$kvs]" \
-    0x000000000000000000000000000000000000000000000000000000002f809dc4
+    0x00000000000000000000000000000000000000000000000000000000e9867806
